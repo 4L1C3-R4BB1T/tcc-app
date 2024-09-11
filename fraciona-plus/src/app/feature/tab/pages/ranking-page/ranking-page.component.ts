d36e46d8221948c899b22ff746161d46b466a465
Livia <livinha.guimaraes.013@gmail.com>
2024-09-10 22:17:09 -0300

    fix: adjustments

--- fraciona-plus/src/app/feature/tab/pages/ranking-page/ranking-page.component.ts
+++ fraciona-plus/src/app/feature/tab/pages/ranking-page/ranking-page.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, QueryList, ViewChildren } from '@angular/core';
+import { AnimationController } from '@ionic/angular';
 
 @Component({
-  selector: 'app-ranking-page',
+  selector: 'app-ranking',
   templateUrl: './ranking-page.component.html',
   styleUrls: ['./ranking-page.component.scss'],
 })
-export class RankingPageComponent {
+export class RankingPageComponent implements AfterViewInit {
+
+  @ViewChildren('personRank')
+  children!: QueryList<ElementRef>;
+
+  constructor(
+    public readonly animationController: AnimationController
+  ) { }
+
+  public ngAfterViewInit(): void {
+    this.children?.forEach((e, index) => this.animateElement(e.nativeElement, (index + 1) * 500));
+  }
+
+  public animateElement(e: HTMLElement, duration: number) {
+    this.animationController.create()
+      .addElement(e)
+      .fromTo('transform', 'translateY(2rem)', 'translateY(0)')
+      .fromTo('opacity', '0', '1')
+      .easing('ease-in-out')
+      .duration(duration)
+      .fill('forwards')
+      .play();
+  }
 
 }
--- fraciona-plus/src/app/feature/tab/tab.module.ts
+++ fraciona-plus/src/app/feature/tab/tab.module.ts
@@ -1,13 +1,14 @@
-import { CUSTOM_ELEMENTS_SCHEMA, NgModule, signal } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { SharedModule } from 'src/app/shared/shared.module';
 import { UserInfoPanelComponent } from './components/user-info-panel/user-info-panel.component';
 import { ChallengePageComponent } from './pages/challenge-page/challenge-page.component';
 import { HomePageComponent } from './pages/home-page/home-page.component';
+import { TrailProgressComponent } from './pages/learn-page/components/trail-progress/trail-progress.component';
 import { LearnPageComponent } from './pages/learn-page/learn-page.component';
 import { PerfilPageComponent } from './pages/perfil-page/perfil-page.component';
+import { RankingPageComponent } from './pages/ranking-page/ranking-page.component';
 import { TabRoutingModule } from './tab-routing.module';
 import { TabComponent } from './tab.component';
-import { TrailProgressComponent } from './pages/learn-page/components/trail-progress/trail-progress.component';
 
 @NgModule({
   declarations: [
@@ -15,6 +16,7 @@ import { TrailProgressComponent } from './pages/learn-page/components/trail-prog
     HomePageComponent,
     LearnPageComponent,
     ChallengePageComponent,
+    RankingPageComponent,
     PerfilPageComponent,
     UserInfoPanelComponent,
     TrailProgressComponent
