1e2e695e154b22e80712055d1314cafb75219e5c
Livia <livinha.guimaraes.013@gmail.com>
2024-09-10 23:30:13 -0300

    feat: profile

--- fraciona-plus/src/app/feature/tab/pages/perfil-page/perfil-page.component.ts
+++ fraciona-plus/src/app/feature/tab/pages/perfil-page/perfil-page.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-perfil-page',
   templateUrl: './perfil-page.component.html',
-  styleUrls: ['./perfil-page.component.scss'],
+  styleUrls: ['./perfil-page.component.scss']
 })
 export class PerfilPageComponent {
 
