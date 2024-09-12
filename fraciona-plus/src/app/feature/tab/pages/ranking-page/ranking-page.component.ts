dbfa9b730d99f837711a3e9313e363b95b5ae666
Livia <livinha.guimaraes.013@gmail.com>
2024-09-11 22:45:58 -0300

    feat: ranking

--- fraciona-plus/src/app/feature/tab/pages/ranking-page/ranking-page.component.ts
+++ fraciona-plus/src/app/feature/tab/pages/ranking-page/ranking-page.component.ts
@@ -1,33 +1,66 @@
-import { AfterViewInit, Component, ElementRef, QueryList, ViewChildren } from '@angular/core';
-import { AnimationController } from '@ionic/angular';
+import { Component } from '@angular/core';
+
+export type User = {
+  name: string;
+  exp: number;
+}
 
 @Component({
   selector: 'app-ranking',
   templateUrl: './ranking-page.component.html',
   styleUrls: ['./ranking-page.component.scss'],
 })
-export class RankingPageComponent implements AfterViewInit {
+export class RankingPageComponent {
 
-  @ViewChildren('personRank')
-  children!: QueryList<ElementRef>;
-
-  constructor(
-    public readonly animationController: AnimationController
-  ) { }
-
-  public ngAfterViewInit(): void {
-    this.children?.forEach((e, index) => this.animateElement(e.nativeElement, (index + 1) * 500));
-  }
-
-  public animateElement(e: HTMLElement, duration: number) {
-    this.animationController.create()
-      .addElement(e)
-      .fromTo('transform', 'translateY(2rem)', 'translateY(0)')
-      .fromTo('opacity', '0', '1')
-      .easing('ease-in-out')
-      .duration(duration)
-      .fill('forwards')
-      .play();
-  }
+  users: User[] = [
+    {
+      name: 'Livia',
+      exp: 1538
+    },
+    {
+      name: 'Gabriel 1',
+      exp: 1537
+    },
+    {
+      name: 'Gabriel 2',
+      exp: 1536
+    },
+    {
+      name: 'Gabriel 3',
+      exp: 1535
+    },
+    {
+      name: 'Gabriel 4',
+      exp: 1534
+    },
+    {
+      name: 'Gabriel 5',
+      exp: 1533
+    },
+    {
+      name: 'Gabriel 6',
+      exp: 1532
+    },
+    {
+      name: 'Gabriel 7',
+      exp: 1531
+    },
+    {
+      name: 'Gabriel 8',
+      exp: 1530
+    },
+    {
+      name: 'Gabriel 9',
+      exp: 1529
+    },
+    {
+      name: 'Gabriel 10',
+      exp: 1528
+    },
+    {
+      name: 'Gabriel 11',
+      exp: 1527
+    }
+  ];
 
 }
